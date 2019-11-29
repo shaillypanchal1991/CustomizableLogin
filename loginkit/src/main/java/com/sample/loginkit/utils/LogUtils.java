0e2a65e3a40c32c540640cdab5cfca77588c705c
Shailly Panchal <shailly.panchal@ril.com>
2019-11-29 14:07:47 -0500

    Refactored Code. Added Analytics

--- loginkit/src/main/java/com/sample/loginkit/utils/LogUtils.java
+++ loginkit/src/main/java/com/sample/loginkit/utils/LogUtils.java
@@ -11,7 +11,7 @@ Common class for overriding Logs to enable or disable them
 public class LogUtils {
 
     public static void debug(final String tag, String message) {
-        if (RootLoginController.getCustomConfiguration().isLoggingEnable) {
+        if (RootLoginController.customConfiguration.isLoggingEnable) {
             Log.d(tag, message);
         }
     }
